a4381df609bd1d3a594f73bfb1dd6daeaae7d5e3
dbeccaria826 <beccdaniel@gmail.com>
2020-10-21 15:41:49 -0400

    Added initial files, simple webpack set up, and images

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # faq-accordion-react
+## Workflow/Stack
+
+- React
+- Webpack
+- Babel
+- CSS Animations
+
+## Build
+
+Challenge is to build a faq accordion card with React(my choice) getting it as close to the design as possible.
+
+## Objectives
+
+- View the optimal layout for the component depending on their device's screen size.
+- See hover states for all itneractive elements on the page.
+- Hide/Show the answer to a question when the question is clicked.
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+  ],
+  entry: [
+    '@babel/polyfill/noConflict',
+    path.join(__dirname, 'src', 'index.js'),
+  ],
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    hot: true,
+    historyApiFallback: true,
+    open: true,
+    clientLogLevel: 'silent',
+    port: 8080,
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(svg|png|jpg|gif|pdf)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'imgs',
+          },
+        },
+      },
+    ],
+  },
+};
