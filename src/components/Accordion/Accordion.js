dd5439f689e883fd97d01e3777f3e979638708be
dbeccaria826 <beccdaniel@gmail.com>
2020-10-22 18:13:13 -0400

    Feat: have basic faq functionality working

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,17 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Slider from '../Slider/Slider';
 import styles from './Accordion.module.css';
 import womanDesktop from '../../images/illustration-woman-online-desktop.svg';
 
 const Accordion = () => {
-  const [clicked, setClicked] = useState(false);
 
-  const handleClick = (e) => {
-    setClicked(clicked => !clicked);
-  
-    console.log(clicked)
-    e.preventDefault();
-  };
   
   return (
     <div className={styles.Container}>
@@ -21,7 +14,13 @@ const Accordion = () => {
       <div className={styles.FaqContainer}>
         <h1 className={styles.FaqTitle}>FAQ</h1>
         <ul>
-          <Slider handleClick={handleClick} text="How many team members can I invite?" />
+          <Slider text="How many team members can I invite?" copy=" You can invite up to 2 additional users on the Free plan. There is no limit on 
+  team members for the Premium plan." />
+          <Slider text="What is the maximum file upload size?" copy="No more than 2GB. All files in your account must fit your allotted storage space." />
+          <Slider text="How do I reset my password?" copy=" Click “Forgot password” from the login page or “Change password” from your profile page.
+  A reset link will be emailed to you." />
+          <Slider text="Can I cancel my subscription?" copy="Yes! Send us a message and we’ll process your request no questions asked." />
+          <Slider text="Do you provide additional support?" copy="Chat and email support is available 24/7. Phone lines are open during normal business hours." />
         </ul>
       </div>
     </div>
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Slider.module.css';
 import iconArrow from '../../images/icon-arrow-down.svg';
 
-const Slider = ({ text, handleClick }) => {
+const Slider = ({ text, copy }) => {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = (e) => {
+    setClicked(clicked => !clicked);
+  
+    e.preventDefault();
+  };
+  console.log(clicked);
   return (
     <>
         <li className={styles.ListChild}>
           {text}
-          <button onClick={handleClick}>
-           ClickMe
-          </button>
+          <button className={styles.ListButton} onClick={handleClick}>
+          <img className={styles.ListIcon} src={iconArrow} alt="arrow down" />
+        </button>
+        {clicked ? <p>{copy}</p> : ''}
         </li>
     </>
   )
