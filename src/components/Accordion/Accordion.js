305293030df10eb61df37ecc7e28797d4b11af6d
dbeccaria826 <beccdaniel@gmail.com>
2020-10-22 22:14:34 -0400

    Feat: slider component now rendered by map function

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from '../Slider/Slider';
 import styles from './Accordion.module.css';
 import womanDesktop from '../../images/illustration-woman-online-desktop.svg';
 
 const Accordion = () => {
-
+  const [showAll, setShowAll] = useState('');
   
   return (
     <div className={styles.Container}>
@@ -14,13 +14,7 @@ const Accordion = () => {
       <div className={styles.FaqContainer}>
         <h1 className={styles.FaqTitle}>FAQ</h1>
         <ul>
-          <Slider text="How many team members can I invite?" copy=" You can invite up to 2 additional users on the Free plan. There is no limit on 
-  team members for the Premium plan." />
-          <Slider text="What is the maximum file upload size?" copy="No more than 2GB. All files in your account must fit your allotted storage space." />
-          <Slider text="How do I reset my password?" copy=" Click “Forgot password” from the login page or “Change password” from your profile page.
-  A reset link will be emailed to you." />
-          <Slider text="Can I cancel my subscription?" copy="Yes! Send us a message and we’ll process your request no questions asked." />
-          <Slider text="Do you provide additional support?" copy="Chat and email support is available 24/7. Phone lines are open during normal business hours." />
+          <Slider />
         </ul>
       </div>
     </div>
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -2,24 +2,54 @@ import React, { useState } from 'react';
 import styles from './Slider.module.css';
 import iconArrow from '../../images/icon-arrow-down.svg';
 
-const Slider = ({ text, copy }) => {
+const Slider = () => {
   const [clicked, setClicked] = useState(false);
+
+  const questions = [
+    {
+      id: 0,
+      text: 'How many team members can I invite?',
+      copy: 'You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.'
+    },
+    {
+      id:1,
+      text: 'What is the maximum file upload size?',
+      copy: 'No more than 2GB. All files in your account must fit your allotted storage space.'
+    },
+    {
+      id:2,
+      text: 'How do I reset my password?',
+      copy: 'Click “Forgot password” from the login page or “Change password” from your profile page. A reset link with be mailed to you.'
+    },
+    {
+      id:3,
+      text: 'Can I cancel my subscription?',
+      copy: 'Yes! Send us a message and we\'ll process your request no questions asked.',
+    },
+    {
+      id: 4,
+      text: 'Do you provide additional support?',
+      copy: 'Chat and email support is available 24/7. Phone lines are open during normal business hours.'
+    },
+  ];
+
 
   const handleClick = (e) => {
     setClicked(clicked => !clicked);
-  
     e.preventDefault();
   };
-  console.log(clicked);
+
   return (
     <>
-        <li className={styles.ListChild}>
-          {text}
+      {questions.map(question => {
+        return <li className={styles.ListChild} key={question.id}>
+          {question.text}
           <button className={styles.ListButton} onClick={handleClick}>
-          <img className={styles.ListIcon} src={iconArrow} alt="arrow down" />
-        </button>
-        {clicked ? <p>{copy}</p> : ''}
+          <img className={styles.ListIcon} src={iconArrow} alt="arrow down" />  
+          </button>
+          {clicked ? <p className={styles.CopyText}>{question.copy}</p> : ''}
         </li>
+      })}
     </>
   )
 };
